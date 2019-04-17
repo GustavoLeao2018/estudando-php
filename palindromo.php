98c10382cfcc17b30a5889c6f962377e86172cff
GustavoLeao2018 <gustavo.leao.nogueira.2017@gmail.com>
2019-04-16 23:05:34 -0300

    Terminado palindromo!

--- palindromo.php
+++ palindromo.php
@@ -1,26 +1,44 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+	<meta charset="utf-8"/>
+	
+	<title>Palindromo</title>
+
+</head>
+<body>
 <?php
 	
-	$palavra  = $_GET["palavra"];
-	$palavra = trim($palavra);
-	$palavra_invertida = strrev($palavra);
-	var_dump($palavra);
+	if(isset($_GET["palavra"])){
+		$palavra  = $_GET["palavra"];
+		$palavra = trim($palavra);
+		$palavra_invertida = strrev($palavra);
+		var_dump($palavra);
 
-	print "<p>Teste de palavras que são palindromos!</p>";
-	
-	print "<p>A palavra digita foi: <b>";	
-	print $palavra;
-	print "</b></p>";
+		print "<p>Teste de palavras que são palindromos!</p>";
+		
+		print "<p>A palavra digita foi: <b>";	
+		print $palavra;
+		print "</b></p>";
 
-	print "<p>A palavra ao contrario:<b>";
-	print $palavra_invertida;	
-	print "</b></p>";
+		print "<p>A palavra ao contrario:<b>";
+		print $palavra_invertida;	
+		print "</b></p>";
 
-	if ($palavra == $palavra_invertida){
-		print "<p>A palavra <b>é</b> um <b>palindromo</b>!</p>";	
+		if ($palavra == $palavra_invertida){
+			print "<p>A palavra <b>é</b> um <b>palindromo</b>!</p>";	
+		}
+		else {
+			print "<p>A palavra <b>não é</b> um <b>palindromo</b>!</p>";	
+		}
 	}
 	else {
-		print "<p>A palavra <b>não é</b> um <b>palindromo</b>!</p>";	
+		print "<p>Digite uma palavra na url com o formato:</p>";
+		print "<p><code>localhost/palindromo.php?palavra=palavra</code></p>";	
 	}
-
 	
 ?>
+
+	<p> Para testar outra palavra cloque <a href="chamar-palindromo.html">aqui</a>.</p>
+</body>
+</html>
